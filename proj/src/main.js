e9aba9adc3bf376c0bbaaf29741fb8bc24434eca
Ale-1407 <alessiacarrera07@gmail.com>
2022-12-05 11:43:29 +0100

    prima parte header

--- proj/src/main.js
+++ proj/src/main.js
@@ -3,6 +3,24 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+  /* import the fontawesome core */
+  import { library } from '@fortawesome/fontawesome-svg-core'
+
+  /* import font awesome icon component */
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+  /* import specific icons */
+  import { faPhone, faClock, faEnvelope, faLocationDot } from '@fortawesome/free-solid-svg-icons'
+
+  /*Import icone brands */
+  import { faFacebookF, faTwitter, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+
+  /* add icons to the library */
+  library.add(faPhone, faClock, faEnvelope, faLocationDot, faFacebookF, faTwitter, faLinkedinIn)
+
+  /* add font awesome icon component */
+  Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
